1e695d5d627e05845600fa6e2d600d58ae52162f
RyutoOkamura <86783195+RyutoOkamura@users.noreply.github.com>
2021-12-17 00:53:00 +0900

    second-commit

--- resources/views/complete.blade.php
+++ resources/views/complete.blade.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
 <body>
     <div class="text-center">
         <h1 class="text-center mt-2 mb-5">お問い合わせありがとうございました。</h1>
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -22,6 +22,7 @@
   <meta property="og:url" content="https://........">
   <title>Contact</title>
   <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <link rel="stylesheet" href="css/style.css">
 </head>
 
